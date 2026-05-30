cdd0ee6e42ba18868bf2e9fdbe62724f086ac8c6
RinJuSu <ojerik74@gmail.com>
2026-05-30 23:08:08 +0700

    Health Bar Animation

--- Scenes/health_bar.gd
+++ Scenes/health_bar.gd
@@ -1,6 +1,7 @@
 extends ProgressBar
 
 var enemy_parent
+var current_tracked_health: float =  -1.0
 
 func _ready() -> void:
 	enemy_parent = get_parent()
@@ -10,8 +11,16 @@ func _ready() -> void:
 	
 func _process(_delta: float) -> void:
 	if enemy_parent != null and "health" in enemy_parent:
-		self.value = enemy_parent.health
+		var actual_health = enemy_parent.health
+
+		if current_tracked_health == -1.0:
+			self.value = actual_health
+			current_tracked_health = actual_health
 		
+		elif current_tracked_health != actual_health:
+			var tween = create_tween()
+			tween.tween_property(self, "value", actual_health, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+			current_tracked_health = actual_health
 		if enemy_parent.health == enemy_parent.health_max or enemy_parent.health <= enemy_parent.health_min:
 			self.visible = false
 		else:
--- Scripts/playerhealth.gd
+++ Scripts/playerhealth.gd
@@ -4,14 +4,25 @@ var parent
 var max_value_amount
 var min_value_amount
 
+var current_tracked_health: float =  -1.0
+
 func _ready() -> void:
 	pass
 	
 func _process(_delta: float) -> void:
 	if Global.playerBody != null:
 		self.max_value = Global.playerBody.health_max
-		self.value = Global.playerBody.health
+		var actual_health = Global.playerBody.health
 		
+		if current_tracked_health == -1.0:
+			self.value = actual_health
+			current_tracked_health = actual_health
+		
+		elif current_tracked_health != actual_health:
+			var tween = create_tween()
+			tween.tween_property(self, "value", actual_health, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+			current_tracked_health = actual_health
+	
 		if Global.playerBody.health == Global.playerBody.health_max or Global.playerBody.health <= Global.playerBody.health_min:
 			self.visible = false
 		else:
