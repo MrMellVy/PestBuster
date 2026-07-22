ad4de230acf5034d5ad7acfb1fea4db30c6a211b
RinJuSu <ojerik74@gmail.com>
2026-07-22 18:57:29 +0700

    CleanUp!

--- Scripts/currentscore_label.gd
+++
@@ -1,7 +0,0 @@
-extends RichTextLabel
-
-var default_text = "CURRENT SCORE: "
-
-func _process(delta: float) -> void:
-	var text = str(default_text, str(Global.current_score))
-	self.text = (text)
--- Scripts/middlecurrentwave_label.gd
+++
@@ -1,8 +0,0 @@
-extends RichTextLabel
-
-
-var default_text = "WAVE "
-
-func _process(delta: float) -> void:
-	var text = str(default_text, str(Global.current_wave))
-	self.text = (text)
