6449cde7406f8fd84434ea77389e17ac2e2013ca
RinJuSu <ojerik74@gmail.com>
2026-08-04 15:55:00 +0700

    [Let's Go] Fix Sounds Settings Layout

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -87,18 +87,18 @@ func _change_camera(choose_camera: Camera2D) -> void:
 		TransitionTween.kill()
 	TransitionTween = create_tween()
 	var target_transform: Transform2D = choose_camera.global_transform
-	TransitionTween.tween_property(transition_camera, "global_transform", target_transform, 0.5).set_trans(Tween.TRANS_SINE)
+	TransitionTween.tween_property(transition_camera, "global_transform", target_transform, 1).set_trans(Tween.TRANS_SINE)
 	
 	if TransitionZoomTween:
 		TransitionZoomTween.kill()
 	TransitionZoomTween = create_tween()
 	var target_zoom: Vector2 = choose_camera.zoom
-	TransitionZoomTween.tween_property(transition_camera, "zoom", target_zoom, 0.5).set_trans(Tween.TRANS_SINE)
+	TransitionZoomTween.tween_property(transition_camera, "zoom", target_zoom, 1).set_trans(Tween.TRANS_SINE)
 	
 	if TransitionOffsetTween:
 		TransitionOffsetTween.kill()
 	TransitionOffsetTween = create_tween()
 	var target_offset: Vector2 = choose_camera.offset
-	TransitionOffsetTween.tween_property(transition_camera, "offset", target_offset, 0.5).set_trans(Tween.TRANS_SINE)
+	TransitionOffsetTween.tween_property(transition_camera, "offset", target_offset, 1).set_trans(Tween.TRANS_SINE)
 
 	selected_camera = choose_camera
